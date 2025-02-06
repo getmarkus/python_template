0e2526d0d00478c490fda1e7f9d48072401deaf1
Chris Markus <chris@christophermarkus.com>
2025-02-06 14:14:37 -0500

    feat: implement application factory and update dependencies

--- app/interface_adapters/endpoints/issues.py
+++ app/interface_adapters/endpoints/issues.py
@@ -1,40 +1,26 @@
 from typing import Annotated
 
+from dependency_injector.wiring import inject
 from fastapi import APIRouter, Depends
 from loguru import logger
-from sqlmodel import Session
 
 from app.core.ports.repositories.issues import IssueRepository
 from app.core.usecases.analyze_issue import AnalyzeIssue
 from app.domain.issue import Issue
-from app.interface_adapters.exceptions import UnsupportedOperationException
-from app.resource_adapters.persistence.in_memory.issues import InMemoryIssueRepository
-from app.resource_adapters.persistence.sqlmodel.database import get_db
-from app.resource_adapters.persistence.sqlmodel.issues import SQLModelIssueRepository
-from config import settings
+from app.interface_adapters.containers import Container
 
 router = APIRouter()
 
 
-# https://fastapi.tiangolo.com/tutorial/dependencies/
-def configure_repository(
-    session: Annotated[Session, Depends(get_db)]
-) -> IssueRepository:
-    execution_mode = settings.execution_mode
-    if execution_mode == "in-memory":
-        return InMemoryIssueRepository()
-    elif execution_mode == "sqlmodel":
-        return SQLModelIssueRepository(session)
-    else:
-        raise UnsupportedOperationException(
-            message="Unsupported repository configuration",
-            detail=f"Execution mode '{execution_mode}' is not supported",
-        )
+def get_repository() -> IssueRepository:
+    return Container.issue_repository()
 
 
 @router.post("/issues/{issue_number}/analyze", response_model=Issue)
+@inject
 def analyze_issue(
-    issue_number: int, repo: Annotated[IssueRepository, Depends(configure_repository)]
+    issue_number: int,
+    repo: Annotated[IssueRepository, Depends(get_repository)],
 ) -> Issue:
     logger.info(f"analyzing issue: {issue_number}")
     use_case = AnalyzeIssue(issue_number=issue_number, repo=repo)
